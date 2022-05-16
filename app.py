2efa796336ce312cc3102397e4ae05d4d1ff7380
fmatheiu98 <matheiuflavius@gmail.com>
2022-05-16 22:37:56 +0300

    Added basic XML-RPC functionality.

--- app.py
+++ app.py
@@ -144,7 +144,6 @@ api.add_resource(Stock, '/stock/<component_id>')
 server = ServerThread()
 server.start()
 
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
