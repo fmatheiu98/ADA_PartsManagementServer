e994f4f6f540f3617fa7775dc4860d3bbdb02cb1
fmatheiu98 <matheiuflavius@gmail.com>
2022-05-16 20:05:59 +0300

    Added basic XML-RPC functionality.

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class ServerThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.daemon = True
-        self.localServer = SimpleXMLRPCServer(("localhost", 3000), logRequests=True)
+        self.localServer = SimpleXMLRPCServer(("https://server-parts-ada.herokuapp.com", 8080), logRequests=True)
         #component = Component()
         #self.localServer.register_function(component.get, name="get1")
         #components = Components()
